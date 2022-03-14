4a804811d0a1bfa3238b00a8748ad33f0b570989
Tim La <tlamarketing111@gmail.com>
2022-03-14 16:28:46 -0400

    aligned items with css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+      <div className="form-container">
+        <label>Task Name:</label>
+        <input type="text" id="task" />
+        <label>Time:</label>
+        <input type="text" id="time" />
+      </div>
     </div>
   );
 }
