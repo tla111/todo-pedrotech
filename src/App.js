76bd8f574c2ca65a57646bdd7cb7b5ac5d8acdd4
Tim La <tlamarketing111@gmail.com>
2022-03-14 16:39:22 -0400

    add css for task component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { useState } from 'react';
 import './App.css';
+import Task from './Components/Task';
 
 function App() {
   const [taskName, setTaskName] = useState("");
   const [time, setTime] = useState("");
   const [taskList, setTaskList] = useState([]);
 
+  const addTask = () => {
+    setTaskList([...taskList, { task: taskName, time: time }])
+    setTaskName("")
+    setTime("")
+  }
 
   return (
     <div className="App">
@@ -19,6 +25,8 @@ function App() {
       </div>
 
       <button onClick={addTask}>Add+</button>
+
+      <Task taskName="do homework" time="20 minutes" />
     </div>
   );
 }
