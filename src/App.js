f68cbda8bdefd032f6a2f4a546449972e85460b6
Tim La <tlamarketing111@gmail.com>
2022-03-14 16:32:23 -0400

    added onchange functions

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [taskName, setTaskName] = useState("");
+  const [time, setTime] = useState("");
+  const [taskList, setTaskList] = useState([]);
+
+
   return (
     <div className="App">
       <h1>Todo List</h1>
+
       <div className="form-container">
         <label>Task Name:</label>
-        <input type="text" id="task" />
+        <input type="text" id="task" onChange={(e) => setTaskName(e.target.value)} />
         <label>Time:</label>
-        <input type="text" id="time" />
+        <input type="text" id="time" onChange={(e) => setTime(e.target.value)} />
       </div>
+
+      <button onClick={addTask}>Add+</button>
     </div>
   );
 }
